4ea60afb1f92b66e531ae959e8ccc21c551fb148
Walter Cavalcanti <waltercavalcanti@gmail.com>
2025-06-03 10:09:36 -0300

    Versiona scripts de criação das tabelas

---
+++ scripts/CREATE_TABLE_Ticket.sql
@@ -0,0 +1,36 @@
+USE [ParkingOnlineDB]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Ticket](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DataEntrada] [datetime] NOT NULL,
+	[DataSaida] [datetime] NULL,
+	[Valor] [decimal](18, 2) NULL,
+	[VeiculoId] [int] NOT NULL,
+	[VagaId] [int] NOT NULL,
+ CONSTRAINT [PK_Ticket] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_Vaga] FOREIGN KEY([VagaId])
+REFERENCES [dbo].[Vaga] ([Id])
+GO
+
+ALTER TABLE [dbo].[Ticket] CHECK CONSTRAINT [FK_Ticket_Vaga]
+GO
+
+ALTER TABLE [dbo].[Ticket]  WITH CHECK ADD  CONSTRAINT [FK_Ticket_Veiculo] FOREIGN KEY([VeiculoId])
+REFERENCES [dbo].[Veiculo] ([Id])
+GO
+
+ALTER TABLE [dbo].[Ticket] CHECK CONSTRAINT [FK_Ticket_Veiculo]
+GO
---
+++ scripts/CREATE_TABLE_Veiculo.sql
@@ -0,0 +1,28 @@
+USE [ParkingOnlineDB]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Veiculo](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Marca] [varchar](100) NULL,
+	[Modelo] [varchar](100) NULL,
+	[Placa] [varchar](7) NOT NULL,
+	[ClienteId] [int] NOT NULL,
+ CONSTRAINT [PK_Veiculo] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Veiculo]  WITH CHECK ADD  CONSTRAINT [FK_Veiculo_Cliente] FOREIGN KEY([ClienteId])
+REFERENCES [dbo].[Cliente] ([Id])
+GO
+
+ALTER TABLE [dbo].[Veiculo] CHECK CONSTRAINT [FK_Veiculo_Cliente]
+GO
