f7ccbb2ab70f1762c181fe3be087f155f267f4df
Walter Cavalcanti <waltercavalcanti@gmail.com>
2025-06-17 15:47:38 -0300

    Marcar as propriedades do tipo decimal da TarifaModel como tipo de dados Currency

--- src/ParkingOnline.UI/Models/TarifaModel.cs
+++ src/ParkingOnline.UI/Models/TarifaModel.cs
@@ -6,9 +6,11 @@ public class TarifaModel : BaseModel<int>
 {
     [Display(Name = "Valor Inicial")]
     [Required(AllowEmptyStrings = false, ErrorMessage = "O valor inicial da tarifa é obrigatório.")]
+    [DataType(DataType.Currency)]
     public decimal ValorInicial { get; set; }
 
     [Display(Name = "Valor por Hora")]
     [Required(AllowEmptyStrings = false, ErrorMessage = "O valor por hora da tarifa é obrigatório.")]
+    [DataType(DataType.Currency)]
     public decimal ValorPorHora { get; set; }
 }
