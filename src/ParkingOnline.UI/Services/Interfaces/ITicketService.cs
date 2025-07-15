ee39cf5deae7e830a2752c0dfa730d11aff42505
Walter Cavalcanti <waltercavalcanti@gmail.com>
2025-07-15 16:45:32 -0300

    Add ITicketService interface and TicketService implementation
    This commit introduces a new `ITicketService` interface in `ITicketService.cs` that defines methods for managing tickets, including retrieval, addition, updating, and deletion.
    
    A corresponding `TicketService` class is implemented in `TicketService.cs`, which utilizes `HttpClient` for asynchronous operations to interact with a backend API for ticket management. The methods ensure proper handling of HTTP requests based on the defined operations in the interface.

---
+++ src/ParkingOnline.UI/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,16 @@
+﻿using ParkingOnline.UI.Models;
+
+namespace ParkingOnline.UI.Services.Interfaces;
+
+public interface ITicketService
+{
+    Task<TicketModel> GetTicketByIdAsync(int id);
+
+    Task<IEnumerable<TicketModel>> GetAllTicketsAsync();
+
+    Task AddTicketAsync(TicketModel ticketModel);
+
+    Task UpdateTicketAsync(int id, TicketModel ticketModel);
+
+    Task DeleteTicketAsync(int id);
+}
---
+++ src/ParkingOnline.UI/Services/TicketService.cs
@@ -0,0 +1,41 @@
+﻿using ParkingOnline.UI.Models;
+using ParkingOnline.UI.Services.Interfaces;
+
+namespace ParkingOnline.UI.Services;
+
+public class TicketService(HttpClient httpClient) : ITicketService
+{
+    public async Task AddTicketAsync(TicketModel ticketModel)
+    {
+        using var _ = await httpClient.PostAsJsonAsync("Ticket/Add", ticketModel);
+    }
+
+    public async Task DeleteTicketAsync(int id)
+    {
+        using var _ = await httpClient.DeleteAsync($"Ticket/Delete/{id}");
+    }
+
+    public async Task<IEnumerable<TicketModel>> GetAllTicketsAsync()
+    {
+        var tickets = await httpClient.GetFromJsonAsync<List<TicketModel>>("Ticket/GetAll");
+
+        return tickets ?? [];
+    }
+
+    public async Task<TicketModel> GetTicketByIdAsync(int id)
+    {
+        var ticket = await httpClient.GetFromJsonAsync<TicketModel>($"Ticket/GetById/{id}");
+
+        if (ticket is null)
+        {
+            throw new KeyNotFoundException($"Ticket com o id {id} não encontrado.");
+        }
+
+        return ticket;
+    }
+
+    public async Task UpdateTicketAsync(int id, TicketModel ticketModel)
+    {
+        using var _ = await httpClient.PutAsJsonAsync($"Ticket/Update/{id}", ticketModel);
+    }
+}
